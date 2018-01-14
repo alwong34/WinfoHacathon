bdc91c33ac4c6a8a613e1338c71220bc10b33ce9
Quezan <quezanw@gmail.com>
2018-01-13 17:51:07 -0800

    updates to profile page and task tools

--- js/task_tool.js
+++ js/task_tool.js
@@ -3,8 +3,23 @@ $(document).ready(function() {
 
   $("#submit-task").on("click", function() {
     var taskVal = $("#task-value").val();
-    taskArray.push(taskVal);
+    var dateVal = $("#date-value").val();
+    var dropVal = $("#priority-value").val();
+
+    var taskObj = {
+      task: taskVal,
+      date: dateVal,
+      priority: dropVal
+    };
+
+    taskArray.push(taskObj);
     console.log(taskArray);
+    updateTasks(taskObj);
   });
+
+  function updateTasks(object) {
+    var html = "<div class='task'><p class='task-name'>" + object.task + "</p> <p class='due-date'>" + object.date + "</p><button class='cancel-btn'>X</button></div>";
+    $("#task-container").append(html);
+  }
 
 });
