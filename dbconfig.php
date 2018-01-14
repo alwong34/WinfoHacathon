621286739bd4c4b678ea77431ca3ac9e540c141c
Alexander Wong <alwong34@uw.edu>
2018-01-13 18:15:00 -0800

    removed old authentication

--- dbconfig.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$username = 'root';
-	$password = 'root';
-	$dbname = 'paphi_website_db';
-	$host = 'localhost';
-?>
--- nav.php
+++
@@ -1,8 +0,0 @@
-<nav align="center">
-	<!-- these are currently links to different pages with forms,
-		should eventually just display form on existing page when
-		button is clicked -->
-	<a href=".\index.php">Home</a>
-	<a href=".\login.php">Login</a>
-	<a href=".\register.php">Register</a>
-</nav>
